6955be6908cc975dc8fa3c0ea74cf85a3ff1a4c8
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-01-03 23:44:33 -0500

    Added predict POST route that feeds data to Clarifai's API. TODO: Clean up and write README

--- lesson-3/routes/index.js
+++ lesson-3/routes/index.js
@@ -5,21 +5,30 @@ const router = express.Router();
 
 // Initialize the API key
 const app = new Clarifai.App({
-    apiKey: 'db50b744e1a647f99be4674ede034ad8' // this is aliya's personal key plz don't use
+    apiKey: 'REPLACE WITH API KEY' // this is aliya's personal key plz don't use
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-app.models.predict(Clarifai.GENERAL_MODEL, "https://cbsnews2.cbsistatic.com/hub/i/r/2015/02/27/dc63bf50-05ee-4733-9217-4718ee9c81fe/resize/620x465/cb60f988990627112be9a03525f66c34/labrador-retriever1istock.jpg").then(
-    function(response) {
-        // do something with response
-    },
-    function(err) {
-        // there was an error
-    }
-);
+/* POST handle form input */
+router.post('/predict', function(req, res, next) {
+  const image = req.body.image;
 
+  app.models.predict(Clarifai.GENERAL_MODEL, image).then(
+      function(response) {
+        const concepts = response.outputs[0].data.concepts;
+
+        res.render('predict', { concepts, image });
+      },
+      function(err) {
+        console.log(err);
+
+        res.redirect('/');
+      }
+  );
+});
 
 module.exports = router;
