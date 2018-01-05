bb1d730523f2d4919a22c7fe7602df2b9d3b8358
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-01-04 20:30:25 -0500

    Added comments to routes to explain what's going on

--- lesson-3/lesson-3-complete/routes/index.js
+++ lesson-3/lesson-3-complete/routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const apiKey = require('../config/clarifai.json').key;
 
-// Initialize the API key
+// Initialize the API key (Fill out your API key in config/clarifai.json)
 const app = new Clarifai.App({
-    apiKey: apiKey // this is aliya's personal key plz don't use
+    apiKey: apiKey
 });
 
 /* GET home page. */
@@ -18,11 +18,16 @@ router.get('/', function(req, res, next) {
 router.post('/predict', function(req, res, next) {
   const image = req.body.image;
 
+  // Connect to the Clarifai API using the prediction feature then feed it the URL passed in through our form
   app.models.predict(Clarifai.GENERAL_MODEL, image)
       .then(
         function(response) {
+          // If we get a successful response, we render the predict page 
+
+          // Get the concepts array from the response we're given (see Clarifai API reference)
           const concepts = response.outputs[0].data.concepts;
 
+          // If any of the tagged concepts is 'dog' then we determine that the image is a dog (Simple logic for now)
           let verdict = false;
 
           for (var c of concepts) {
@@ -32,9 +37,11 @@ router.post('/predict', function(req, res, next) {
             }
           }
 
+          // Render our predict page and send it the concepts array, the original image URL, and the final dog or not dog verdict
           res.render('predict', { concepts, image, verdict });
         },
         function(err) {
+          // If we get an error, we render the error page with the following message
           res.render('error', { message: 'Error trying to process that image. Likely a bad URL'});
         }
       );
